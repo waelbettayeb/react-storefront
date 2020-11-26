9a64172224542c44542cfe8864a09f0598b57ee7
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-11-26 15:18:45 +0100

    add size and color to the query

--- pages/api/p/[productId].js
+++ pages/api/p/[productId].js
@@ -1,6 +1,6 @@
 import { product } from 'react-storefront-connector'
 
 export default async function pdp(req, res) {
-  const { productId } = req.query
-  return res.json(await product({ id: productId }, req, res))
+  const { productId, size, color } = req.query
+  return res.json(await product({ id: productId, size, color }, req, res))
 }
